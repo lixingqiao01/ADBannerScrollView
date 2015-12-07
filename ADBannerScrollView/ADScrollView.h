5862a3e94bc444a738898db66d245c7a454cc896
XingQiao Li <lixingqiao01@163.com>
2015-12-07 23:40:27 +0800

    编写UI
    编写UI

--- ADBannerScrollView/ADScrollView.h
+++ ADBannerScrollView/ADScrollView.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIImageView+WebCache.h"
+#define MAINSCREEN_BOUND [UIScreen mainScreen].bounds
 
 @protocol ADScrollViewDelegate <NSObject>
 /**
--- ADBannerScrollView/ViewController.m
+++ ADBannerScrollView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ADScrollView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect rect = [UIScreen mainScreen].bounds;
+    NSArray *imagePath = [NSArray arrayWithObjects:@"http://s0.hao123img.com/res/r/image/2015-12-07/cc437f2c431358be27fd69dfbf5a04f5.jpg",@"http://s0.hao123img.com/res/r/image/2015-12-07/bf0eb0980dd99b3753198e5ac5011feb.jpg",@"http://s0.hao123img.com/res/r/image/2015-12-07/111540120c81e63529e708b6f798031b.jpg",@"http://s0.hao123img.com/res/r/image/2015-12-07/3bc5cb0cbc026f6075e772eeeb998385.jpg", nil];
+    ADScrollView *adView = [[ADScrollView alloc]initWithFrame:CGRectMake(0, 20, rect.size.width, 150) ImageURLArray:imagePath TimeInterval:3];
+    adView.pageController.frame = CGRectMake(0, 150, rect.size.width, 20);
+    [self.view addSubview:adView];
+    [self.view addSubview:adView.pageController];
 }
 
 - (void)didReceiveMemoryWarning {
